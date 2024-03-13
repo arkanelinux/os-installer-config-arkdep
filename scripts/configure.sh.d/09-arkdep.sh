d188b26d30feb9121c1c9eff0d384ed3d6d800d7
Dennis ten Hoove <dennistenhoove@protonmail.com>
2024-03-13 18:23:59 +0100

    Do not chroot mkswapfile

--- scripts/configure.sh.d/09-arkdep.sh
+++ scripts/configure.sh.d/09-arkdep.sh
@@ -11,14 +11,14 @@ declare -ri memtotal=$(grep MemTotal /proc/meminfo | awk '{print $2}')
 # Determine suitable swapfile size
 if [[ $memtotal -lt 4500000 ]]; then
 	# If RAM is less than 4.5GB create a 2GB swapfile
-	sudo arch-chroot $workdir btrfs filesystem mkswapfile --size 2G /arkdep/shared/var/swapfile \
+	sudo btrfs filesystem mkswapfile --size 2G $workdir/arkdep/shared/var/swapfile \
 		|| quit_on_err 'Failed to create swapfile'
 elif [[ $memtotal -lt 8500000 ]]; then
 	# If RAM is less than 8.5GB, create a 4GB swapfile
-	sudo arch-chroot $workdir btrfs filesystem mkswapfile --size 4G /arkdep/shared/var/swapfile \
+	sudo btrfs filesystem mkswapfile --size 4G $workdir/arkdep/shared/var/swapfile \
 		|| quit_on_err 'Failed to create swapfile'
 else
 	# Else create a 6GB swapfile
-	sudo arch-chroot $workdir btrfs filesystem mkswapfile --size 6G /arkdep/shared/var/swapfile \
+	sudo btrfs filesystem mkswapfile --size 6G $workdir/arkdep/shared/var/swapfile \
 		|| quit_on_err 'Failed to create swapfile'
 fi
