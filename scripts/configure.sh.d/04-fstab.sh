7c44ffe56db7f919e0be930a84d6ce4842842e5e
Dennis ten Hoove <dennistenhoove@protonmail.com>
2024-03-13 21:15:00 +0100

    Fix genfstab

--- scripts/configure.sh.d/04-fstab.sh
+++ scripts/configure.sh.d/04-fstab.sh
@@ -2,4 +2,6 @@
 sudo mkdir $workdir/arkdep/overlay/etc
 
 # Update fstab with /boot config
-grep '/boot' $workdir/etc/fstab | sudo tee -a $workdir/arkdep/overlay/etc/fstab
+genfstab $workdir \
+	| grep '/boot' \
+	| sudo tee -a $workdir/arkdep/overlay/etc/fstab
