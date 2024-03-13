0a7abb6fa3318bbc927c89508403ae9cc9903db9
Dennis ten Hoove <dennistenhoove@protonmail.com>
2024-03-13 21:16:12 +0100

    Disable bootctl arkdep

--- scripts/configure.sh.d/09-arkdep.sh
+++ scripts/configure.sh.d/09-arkdep.sh
@@ -1,5 +1,5 @@
 # Deploy latest image
-sudo ARKDEP_ROOT=$workdir arkdep deploy || quit_on_err 'Failed to deploy image with arkdep'
+sudo ARKDEP_NO_BOOTCTL=1 ARKDEP_ROOT=$workdir arkdep deploy || quit_on_err 'Failed to deploy image with arkdep'
 
 # Copy previously generated locale over
 sudo cp -v /usr/lib/locale/locale-archive $workdir/arkdep/shared/var/usrliblocale/ \
--- scripts/install.sh.d/04-initialize-arkdep.sh
+++ scripts/install.sh.d/04-initialize-arkdep.sh
@@ -1,3 +1,3 @@
 # Initialize arkdep
-sudo ARKDEP_ROOT="$workdir" arkdep init \
+sudo ARKDEP_NO_BOOTCTL=1 ARKDEP_ROOT="$workdir" arkdep init \
 	|| quit_on_err 'Failed to init arkep'
