4a179f98cdbe14da7f25eb861a7c65e86221c049
Dennis ten Hoove <dennistenhoove@protonmail.com>
2024-03-13 05:02:29 +0100

    Drop recovery overlay

--- scripts/configure.sh.d/02-overlay.sh
+++ scripts/configure.sh.d/02-overlay.sh
@@ -1,8 +1,3 @@
-# Copy overlay to new root
-for f in $(ls $osidir/overlay/); do
-	sudo cp -rv $osidir/overlay/$f $workdir/ || quit_on_err 'Failed to copy overlay to workdir'
-done
-
 # Write overlay_arkdep
 for f in $(ls $osidir/overlay_arkdep/); do
 	sudo cp -rv $osidir/overlay_arkdep/$f $workdir/arkdep/overlay/
