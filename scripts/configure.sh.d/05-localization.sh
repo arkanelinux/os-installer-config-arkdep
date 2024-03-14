76d617e03825fab58c64706a3516277acb754e28
Dennis ten Hoove <dennistenhoove@protonmail.com>
2024-03-14 02:46:02 +0100

    Fix keymap syntax

--- scripts/configure.sh.d/05-localization.sh
+++ scripts/configure.sh.d/05-localization.sh
@@ -22,4 +22,4 @@ sudo ln -sf /usr/share/zoneinfo/$OSI_TIMEZONE $workdir/arkdep/overlay/etc/localt
 # TODO: Also set in TTY
 declare -r current_keymap=$(gsettings get org.gnome.desktop.input-sources sources)
 sudo mkdir -p $workdir/arkdep/overlay/etc/dconf/db/local.d
-printf "[org.gnome.desktop.input-sources]\nsources = $current_keymap\n" | sudo tee $workdir/arkdep/overlay/etc/dconf/db/local.d/keymap || quit_on_err 'Failed to set dconf keymap'
+printf "[org/gnome/desktop/input-sources]\nsources = $current_keymap\n" | sudo tee $workdir/arkdep/overlay/etc/dconf/db/local.d/keymap || quit_on_err 'Failed to set dconf keymap'
