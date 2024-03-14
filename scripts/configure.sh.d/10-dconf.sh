c02d946d61a77cab6bf39a38a1bf42e9ca848389
Dennis ten Hoove <dennistenhoove@protonmail.com>
2024-03-14 03:18:48 +0100

    Add dconf.sh

---
+++ scripts/configure.sh.d/10-dconf.sh
@@ -0,0 +1,9 @@
+declare -r deployment=($(ls $workdir/arkdep/deployments/))
+
+sudo dconf compile $workdir/arkdep/overlay/etc/dconf/db/local \
+	$workdir/arkdep/deployments/${deployment[0]}/rootfs/etc/dconf/db/local.d/ \
+	|| quit_on_err 'Failed to generate local dconf database'
+
+sudo cp $workdir/arkdep/overlay/etc/dconf/db/local \
+	$workdir/arkdep/deployments/${deployment[0]}/rootfs/etc/dconf/db/local \
+	|| quit_on_err 'Failed to copy dconf local db to deployment'
