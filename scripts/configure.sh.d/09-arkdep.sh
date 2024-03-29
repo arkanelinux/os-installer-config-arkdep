e6cbeb818fd56a32446fed8481860e6694d7ac47
Dennis ten Hoove <dennistenhoove@protonmail.com>
2024-03-29 20:59:25 +0100

    Port to new Arkdep version

--- scripts/configure.sh.d/09-arkdep.sh
+++ scripts/configure.sh.d/09-arkdep.sh
@@ -1,8 +1,10 @@
 # Deploy latest image
 sudo ARKDEP_NO_BOOTCTL=1 ARKDEP_ROOT=$workdir arkdep deploy || quit_on_err 'Failed to deploy image with arkdep'
 
+declare -r deployment_version=($(ls $workdir/arkdep/deployments))
+
 # Copy previously generated locale over
-sudo cp -v /usr/lib/locale/locale-archive $workdir/arkdep/shared/var/usrliblocale/ \
+sudo cp -v /usr/lib/locale/locale-archive $workdir/arkdep/deployments/${deployment_version[0]}/rootfs/var/usrliblocale/ \
 	|| quit_on_err 'Failed to copy locale-archive to usrliblocale'
 
 # Collect information about the system memory, this is used to determine an apropriate swapfile size
@@ -11,14 +13,14 @@ declare -ri memtotal=$(grep MemTotal /proc/meminfo | awk '{print $2}')
 # Determine suitable swapfile size
 if [[ $memtotal -lt 4500000 ]]; then
 	# If RAM is less than 4.5GB create a 2GB swapfile
-	sudo btrfs filesystem mkswapfile --size 2G $workdir/arkdep/shared/var/swapfile \
+	sudo btrfs filesystem mkswapfile --size 2G $workdir/arkdep/shared/swapfile \
 		|| quit_on_err 'Failed to create swapfile'
 elif [[ $memtotal -lt 8500000 ]]; then
 	# If RAM is less than 8.5GB, create a 4GB swapfile
-	sudo btrfs filesystem mkswapfile --size 4G $workdir/arkdep/shared/var/swapfile \
+	sudo btrfs filesystem mkswapfile --size 4G $workdir/arkdep/shared/swapfile \
 		|| quit_on_err 'Failed to create swapfile'
 else
 	# Else create a 6GB swapfile
-	sudo btrfs filesystem mkswapfile --size 6G $workdir/arkdep/shared/var/swapfile \
+	sudo btrfs filesystem mkswapfile --size 6G $workdir/arkdep/shared/swapfile \
 		|| quit_on_err 'Failed to create swapfile'
 fi
