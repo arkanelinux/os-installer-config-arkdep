a4d445fcc92002c1317222ba2a4ede0b8dcc3db6
Dennis ten Hoove <dennistenhoove@protonmail.com>
2024-07-08 14:46:50 +0200

    Add rd.auto=0 to kernel params on LUKS

--- scripts/configure.sh.d/06-systemd-boot.sh
+++ scripts/configure.sh.d/06-systemd-boot.sh
@@ -8,7 +8,7 @@ if [[ $OSI_USE_ENCRYPTION == 1 ]]; then
 	initrd /amd-ucode.img
 	initrd /intel-ucode.img
 	initrd /arkdep/%target%/initramfs-linux.img
-	options rd.luks.name=$uuid=arkane_root root=/dev/mapper/arkane_root rootflags=subvol=/arkdep/deployments/%target%/rootfs lsm=landlock,lockdown,yama,integrity,apparmor,bpf quiet splash loglevel=3 systemd.show_status=auto rd.udev.log_level=3 rw
+	options rd.auto=0 rd.luks.name=$uuid=arkane_root root=/dev/mapper/arkane_root rootflags=subvol=/arkdep/deployments/%target%/rootfs lsm=landlock,lockdown,yama,integrity,apparmor,bpf quiet splash loglevel=3 systemd.show_status=auto rd.udev.log_level=3 rw
 	END
 else
 	# Overwrite default Arkdep systemd-boot template
