8975ddcbec8c2abf14abd2584d584200b48f1a03
Dennis ten Hoove <dennistenhoove@protonmail.com>
2025-12-02 20:38:47 +0100

    Code cleanup

--- scripts/configure.sh.d/06-arkdep.sh
+++ scripts/configure.sh.d/06-arkdep.sh
@@ -1,7 +1,5 @@
 # Deploy latest image
-sudo ARKDEP_NO_BOOTCTL=1 ARKDEP_ROOT=$workdir arkdep deploy || quit_on_err 'Failed to deploy image with arkdep'
-
-declare -r deployment_version=($(ls $workdir/arkdep/deployments))
+sudo ARKDEP_ROOT=$workdir arkdep deploy || quit_on_err 'Failed to deploy image with arkdep'
 
 # Collect information about the system memory, this is used to determine an apropriate swapfile size
 declare -ri memtotal=$(grep MemTotal /proc/meminfo | awk '{print $2}')
